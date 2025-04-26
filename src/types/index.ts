bf8bc7cd34ca377bc288cc8933f105f2fb5ee760
Eduardo Função <eduardofuncao@hotmail.com>
2025-04-25 22:47:48 -0300

    Implementação de Perfis no Agendamento de Consultas Médicas

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,21 +1,3 @@
-export type Appointment = {
-  id: string;
-  doctorId: string;
-  date: string;
-  time: string;
-  description: string;
-  status: string;
-};
-
-export type Doctor = {
-  id: string;
-  name: string;
-  specialty: string;
-  image: string;
-};
-
-export type RootStackParamList = {
-  Home: undefined;
-  CreateAppointment: undefined;
-  Profile: undefined;
-}; 
+export { Appointment } from './appointments';
+export { Doctor } from './doctors';
+export { RootStackParamList } from './navigation';
